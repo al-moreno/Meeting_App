3e16ebdb70eeaf56c202eddbad5f07da1f2776cd
Ana Laura Moreno <ana@eurekaprocess.com>
2022-02-02 11:01:17 -0600

    added pending meeting components

--- client/src/Components/IncomingUpdateItem.js
+++ client/src/Components/IncomingUpdateItem.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const IncomingUpdateItem = props => {
     return (
-        <li>{props.incomingupdate.name}</li>
+        <span className="pb-2" >
+            <li className="list-group-item  ">
+
+                <input className="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+
+                {props.incomingupdate.name}
+
+            </li>
+        </span>
     )
 };
 
