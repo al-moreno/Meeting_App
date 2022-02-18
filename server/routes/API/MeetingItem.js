31cc3a2632f9b1d1f60bb8efa43aba35bfb288fd
Ana Laura Moreno <ana@eurekaprocess.com>
2022-02-18 11:06:03 -0600

    delete feature completed

--- server/routes/API/MeetingItem.js
+++ server/routes/API/MeetingItem.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const userRouter = express.Router();
+const meetingRouter = express.Router();
 const passport = require('passport');
 const MeetingItem = require('../../models/MeetingItem');
 
 
-userRouter.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+meetingRouter.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     const meetingitem = new MeetingItem(req.body);
     meetingitem.save(err => {
         if (err)
@@ -15,12 +15,22 @@ userRouter.post('/', passport.authenticate('jwt', { session: false }), (req, res
                 if (err)
                     res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
                 else
-                    res.status(200).json({ message: { msgBody: "Successfully created meeting item", msgError: false }, meetingitem});
+                    res.status(200).json({ message: { msgBody: "Successfully created meeting item", msgError: false }, meetingitem });
             });
         }
     })
 });
 
+meetingRouter.delete('/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        const deletedInfo = await MeetingItem.findByIdAndDelete(req.params.id)
+        res.status(200).json({ message: { msgBody: "Successfully deleted meeting item", msgError: false }, deletedInfo });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
+    }
+});
 
 
-module.exports = userRouter; 
+
+module.exports = meetingRouter; 
